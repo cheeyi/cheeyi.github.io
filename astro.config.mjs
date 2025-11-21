42ac79dace2704f5373fe5108fe5b98f08e03b93
Chee Yi Ong <cheeyi@gmail.com>
2025-11-21 15:06:30 +0800

    feat: brand icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cheeyi.me',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		shikiConfig: {
-			theme: 'github-light',
-			wrap: true,
-		},
-	},
+    site: 'https://cheeyi.me',
+    integrations: [mdx(), sitemap(), icon()],
+    markdown: {
+        shikiConfig: {
+            theme: 'github-light',
+            wrap: true,
+        },
+    },
 });
